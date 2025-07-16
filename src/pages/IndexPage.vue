5546d0792bc1e96fce480ad3ade730b1a6eb45ed
Jakkaphat-03 <audijakkapat03@gmail.com>
2025-07-16 12:33:21 +0700

    edit iIndexPage.vu merge

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="q-pa-md" style="max-width: 400px">
+<<<<<<< HEAD
+=======
+
+>>>>>>> thai-lang
     <q-form
       @submit="onSubmit"
       @reset="onReset"
@@ -8,15 +12,25 @@
       <q-input
         filled
         v-model="name"
+<<<<<<< HEAD
         label="Your name *"
         hint="Name and surname"
         lazy-rules
         :rules="[ val => val && val.length > 0 || 'Please type something']"
       />
+=======
+        label="ชื่อ-สกุล *"
+        hint="ชื่อและนามสกุล"
+        lazy-rules
+        :rules="[ val => val && val.length > 0 || 'กรุณาพิมพ์ชื่อ']"
+      />
+
+>>>>>>> thai-lang
       <q-input
         filled
         type="number"
         v-model="age"
+<<<<<<< HEAD
         label="อายุ"
         lazy-rules
         :rules="[
@@ -32,3 +46,66 @@
     </q-form>
   </div>
 </template>
+=======
+        label="อายุ *"
+        lazy-rules
+        :rules="[
+          val => val !== null && val !== '' || 'กรุณาใส่อายุ',
+          val => val > 0 && val < 100 || 'กรุณาใส่อายุจริง'
+        ]"
+      />
+
+      <q-toggle v-model="accept" label="ยอมรับ" />
+
+      <div>
+        <q-btn label="ยืนยัน" type="submit" color="primary"/>
+        <q-btn label="เริ่มใหม่" type="reset" color="primary" flat class="q-ml-sm" />
+      </div>
+    </q-form>
+
+  </div>
+</template>
+
+<script>
+import { useQuasar } from 'quasar'
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    const $q = useQuasar()
+
+    const name = ref(null)
+    const age = ref(null)
+    const accept = ref(false)
+    return {
+      name,
+      age,
+      accept,
+      onSubmit () {
+        if (accept.value !== true) {
+          $q.notify({
+            color: 'red-5',
+            textColor: 'white',
+            icon: 'warning',
+            message: 'คุณจำเป็นต้องยอมรับ'
+          })
+        }
+        else {
+          $q.notify({
+            color: 'green-4',
+            textColor: 'white',
+            icon: 'cloud_done',
+            message: 'ข้อมูลได้รับการยืนยัน'
+          })
+        }
+      },
+      onReset () {
+        name.value = null
+        age.value = null
+        accept.value = false
+      }
+    }
+  }
+}
+</script>
+>>>>>>> thai-lang
