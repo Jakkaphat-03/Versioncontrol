000b6d4dfd6fb5aa49c51f3d82f13ce84e92dab7
Jakkaphat-03 <audijakkapat03@gmail.com>
2025-08-14 10:38:27 +0700

    แก้ไขภายใน submodule

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,84 +1,3 @@
-<template>
-  <div class="q-pa-md" style="max-width: 400px">
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
->>>>>>> thai-lang
-=======
-
->>>>>>> thai-lang
-    <q-form
-      @submit="onSubmit"
-      @reset="onReset"
-      class="q-gutter-md"
-    >
-      <q-input
-        filled
-        v-model="name"
-<<<<<<< HEAD
-<<<<<<< HEAD
-        label="Your name *"
-        hint="Name and surname"
-        lazy-rules
-        :rules="[ val => val && val.length > 0 || 'Please type something']"
-      />
-=======
-=======
->>>>>>> thai-lang
-        label="ชื่อ-สกุล *"
-        hint="ชื่อและนามสกุล"
-        lazy-rules
-        :rules="[ val => val && val.length > 0 || 'กรุณาพิมพ์ชื่อ']"
-      />
-
-<<<<<<< HEAD
->>>>>>> thai-lang
-=======
->>>>>>> thai-lang
-      <q-input
-        filled
-        type="number"
-        v-model="age"
-<<<<<<< HEAD
-<<<<<<< HEAD
-        label="อายุ"
-        lazy-rules
-        :rules="[
-          val => val !== null && val !== '' || 'ใส่อายุของคุณนะครับ',
-          val => val > 0 && val < 100 || 'กรุณาใส่อายุของคุณนะจ๊ะ'
-        ]"
-      />
-      <q-toggle v-model="accept" label="I accept the license and terms" />
-      <div>
-        <q-btn label="Submit" type="submit" color="primary"/>
-        <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
-      </div>
-    </q-form>
-  </div>
-</template>
-=======
-=======
->>>>>>> thai-lang
-        label="อายุ *"
-        lazy-rules
-        :rules="[
-          val => val !== null && val !== '' || 'กรุณาใส่อายุ',
-          val => val > 0 && val < 100 || 'กรุณาใส่อายุจริง'
-        ]"
-      />
-
-      <q-toggle v-model="accept" label="ยอมรับ" />
-
-      <div>
-        <q-btn label="ยืนยัน" type="submit" color="primary"/>
-        <q-btn label="เริ่มใหม่" type="reset" color="primary" flat class="q-ml-sm" />
-      </div>
-    </q-form>
-
-  </div>
-</template>
-
 <script>
 import { useQuasar } from 'quasar'
 import { ref } from 'vue'
@@ -100,7 +19,7 @@ export default {
             color: 'red-5',
             textColor: 'white',
             icon: 'warning',
-            message: 'คุณจำเป็นต้องยอมรับ'
+            message: 'You need to accept the license and terms first'
           })
         }
         else {
@@ -108,7 +27,7 @@ export default {
             color: 'green-4',
             textColor: 'white',
             icon: 'cloud_done',
-            message: 'ข้อมูลได้รับการยืนยัน'
+            message: 'Submitted'
           })
         }
       },
@@ -121,4 +40,3 @@ export default {
   }
 }
 </script>
->>>>>>> thai-lang
